e7efd86a94decba39d92f4ca6c792ebc7d674c88
NhiepLy <whitestars932@gmail.com>
2020-06-21 10:48:32 +0700

    BTVN HERUKO

---
+++ Sever2.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+var path = require('path')
+var anh= path.join(__dirname,'./public')
+app .use('/hinhanh', express.static(anh))
+app.get('/me', function(req,res){
+
+  var filehtml = path.join(__dirname,'sever2.html')
+  res.sendFile(filehtml)
+})
+app.listen(9999)
