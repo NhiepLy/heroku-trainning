36bc3ee229b0964acbb579b414fdf44c0cf306df
NhiepLy <whitestars932@gmail.com>
2020-06-21 11:23:07 +0700

    fix link

--- Sever2.js
+++ Sever2.js
@@ -3,11 +3,11 @@ const app = express()
 var path = require('path')
 var anh= path.join(__dirname,'./public')
 app .use('/hinhanh', express.static(anh))
-app.get('/me', function(req,res){
+app.get('/home', function(req,res){
 
   var filehtml = path.join(__dirname,'sever2.html')
   res.sendFile(filehtml)
 })
 app.listen(process.env.PORT, function(){
-  
+
 })
